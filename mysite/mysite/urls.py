914be022230bd84aa51abbb4101c3ca70c2c472b
PoojaLakha <pooja_nl@outlook.com>
2018-12-17 18:56:03 +0530

    signup implementation

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('user/', include('accounts.urls')),
     path('user/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
