2dd5b3094bf7072e68c1cf9c58a418ba6f034a14
PoojaLakha <pooja_nl@outlook.com>
2019-02-11 17:58:32 +0530

    never implement me like this -Forms

--- sibtc/boards/views.py
+++ sibtc/boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 # from django.http import HttpResponse
 # from django.http import Http404
-from .models import Board
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -19,3 +20,29 @@ def board_topics(request, pk):
     return render(request, 'topics.html', {'board': board})'''
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)
+
+    return render(request, 'new_topic.html', {'board': board})
